b69ab2c07278143fd3a5a74be0f32b1732432556
paul <paul.j99@web.de>
2018-12-13 12:34:12 +0100

    added design recipe for task 2

--- 08/ch08.hs
+++ 08/ch08.hs
@@ -1,6 +1,6 @@
 import Test.QuickCheck
---Task 1
 
+--Task 1
 data Nat = Zero | Succ Nat deriving (Show)
 
 one = Succ Zero
@@ -15,18 +15,26 @@ natToInt Zero = 0
 natToInt (Succ a) = 1 + natToInt a
 
 --Task2 
---CONTRACT
 add :: Nat -> Nat -> Nat
-
---
 add Zero n = n
 add (Succ m) n = Succ (add m n)
 
+
+--CONTRACT
 mult :: Nat -> Nat -> Nat
 
+--PURPOSE
+--multiplies natural numbers
+
+--EXAMPLES
+--mult one four = four
+--mult Zero _ = Zero
+
+--DEFINITION
 mult Zero _ = Zero
 mult (Succ a)  b = add b (mult a b) 
 
+--TEST
 prop_mult_test = do
     forAll (choose(0,100) :: Gen Int) $ \n ->
         forAll (choose(0,100)::Gen Int) $ \m ->
@@ -46,5 +54,3 @@ eval xs (Leaf a) = a
 eval (x:xs) (Node a b)
             | x = eval xs a
             | otherwise = eval xs b
-
---Task 6
